ef6923fc552dca82bb0aeb287c07553696a22947
Adrien Gregorj <adrien.gregorjm@ensimag.grenoble-inp.fr>
2017-12-05 09:32:19 +0100

    add Cmake and gitignore

---
+++ src/Matrix.hpp
@@ -0,0 +1,24 @@
+#ifndef MATRIX
+#define MATRIX
+
+#include <vector>
+
+class Matrix
+{
+public:
+    Matrix();
+    Matrix(int n, int m, std::vector<double> coefficients);
+    ~Matrix();
+    int getN() const {return m_n};
+    int getM() const {return m_m};
+    
+    double& operator () (int i, int j);
+
+protected:
+    int m_n;
+    int m_m;
+    std::vector<double> m_coefficients;
+
+};
+
+#endif
