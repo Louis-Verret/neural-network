76195e8c22aadd4592c116781b3754b44325f823
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2017-12-05 10:14:38 +0100

    in out param

--- src/NeuralNetwork.cpp
+++ src/NeuralNetwork.cpp
@@ -1,6 +1,13 @@
 #include "NeuralNetwork.h"
 
 
-std::vector<double>& NeuralNetwork::propagate(const std::vector<double>& x) const {
+void NeuralNetwork::propagate(const std::vector<double>& x, std::vector<double>& y) const {
+
+    std::vector<double> w = m_weights_list[0];
+    std::vector<double> y =
+    int n = w.size();
+    for (int i = 0; i < n; i++) {
+
+    }
 
 }
