245ad14a23015200cd986d1658c62947aecea2d7
Adrien Gregorj <adrien.gregorjm@ensimag.grenoble-inp.fr>
2017-12-05 10:14:38 +0100

    in out param

--- src/NeuralNetwork.h
+++ src/NeuralNetwork.h
@@ -11,7 +11,7 @@ public:
 protected:
     std::vector<std::vector<double> > m_weights_list;
 
-    std::vector<double>& propagate(const std::vector<double>& x) const;
+    void propagate(const std::vector<double>& x, std::vector<double>& y) const;
 };
 
 #endif
