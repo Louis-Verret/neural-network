53acc2b42efce5ae80ace32de4d5297807020c9c
Adrien Gregorj <adrien.gregorj@ensimag.grenoble-inp.fr>
2017-12-12 08:13:28 +0100

    bla

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,27 @@
 
 
 int main(int argc, char **argv) {
-   std::vector<std::vector<double> > x;
-   std::vector<std::vector<double> > d;
-   //std::vector<double> x_test;
-   //x_test.push_back(0);
+    std::vector<std::vector<double> > x;
+    std::vector<std::vector<double> > d;
+    //std::vector<double> x_test;
+    //x_test.push_back(0);
 
-   NeuralNetwork *net = new NeuralNetwork();
-   generateData(x, d, 100);
+    NeuralNetwork *net = new NeuralNetwork();
+    generateSinusData(x, d, 100);
 
-   std::string sigmoid ("sigmoid");
-   net->addLayer(5, sigmoid, 1);
-   net->addLayer(5, sigmoid);
-   net->addLayer(1, sigmoid);
+    std::string sigmoid ("sigmoid");
+    net->addLayer(5, sigmoid, 1);
+    net->addLayer(5, sigmoid);
+    net->addLayer(1, sigmoid);
 
-   std::cout << *net;
+    //std::cout << *net;
 
-   //net->fit(x, d, 10000, 0.1);
-   //double output = net->predict(x_test)[0];
-   //std::cout << "Output: " << output << std::endl;
+    //net->fit(x, d, 10000, 0.1);
+
+    net->save("../data/sinus_training.data");
+
+    //double output = net->predict(x_test)[0];
+    //std::cout << "Output: " << output << std::endl;
 
 
     return 0;
