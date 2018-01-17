6a780ad3521014ddee0c1c77ece6c26d4bda2840
Louis Verret <louis.verret@ensimag.grenoble-inp.fr>
2018-01-17 16:55:36 +0100

    centralize normalize method

--- src/Utils.h
+++ src/Utils.h
@@ -6,6 +6,8 @@
 #include "Matrix.h"
 
 void readCSV(const char* file_name, bool header, Matrix& x, Matrix& y);
+void normalizeData(Matrix& x);
+void centralizeData(Matrix& x, double min_value = 0, double max_value = 0);
 void generateSinusData(Matrix& x, Matrix& d, int s);
 void oneHotEncoding(Matrix& y, int n_class);
 
